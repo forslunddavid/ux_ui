f9797443b4d4ab4e9c8bf4a665de140b29b2b3c4
Forslunddavid <forslund.david@gmail.com>
2023-10-24 19:13:47 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+import "./App.css"
+
+function App() {
+	const [count, setCount] = useState(0)
+
+	return (
+		<>
+			<header>
+				<h1>MovieFlex</h1>
+			</header>
+			<h2></h2>
+		</>
+	)
+}
+
+export default App
